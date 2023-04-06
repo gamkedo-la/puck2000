dad18211e7d94761c51c0011bf5f9a7a45831c18
liyiz <5160472+liyiz@users.noreply.github.com>
2023-04-06 17:50:56 +0100

    add classic w/ std puck instantiation in GamePlay.tscn

--- scenes/GamePlay/game_play.gd
+++ scenes/GamePlay/game_play.gd
@@ -5,5 +5,35 @@ extends Spatial
 export (PackedScene) var Table
 export (PackedScene) var Puck
 
+onready var pucks = $Pucks
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_place_table()
+	pass
+
+
+func _place_table() -> void:
+	var table = Table.instance()
+	add_child(table)
+	_place_pucks(table)
+	pass
+
+
+func _place_pucks(table:StaticBody) -> void:
+	var p1_spawn = table.get_node("P1PuckSpawn").get_children()
+	var p2_spawn = table.get_node("P2PuckSpawn").get_children()
+	for positionNode in p1_spawn:
+#		print(pos.transform.origin)
+		var puck = _get_puck_instance(positionNode)
+		pucks.add_child(puck)
+	for positionNode in p2_spawn:
+		var puck = _get_puck_instance(positionNode)
+		pucks.add_child(puck)
+	pass
+
+
+func _get_puck_instance(positionNode:Position3D) -> RigidBody:
+		var puck = Puck.instance()
+		puck.transform.origin = positionNode.transform.origin
+		return puck
