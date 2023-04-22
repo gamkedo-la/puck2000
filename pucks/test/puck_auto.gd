c1b491a001b45f6e8a8718de017c1d997b7b462f
liyiz <5160472+liyiz@users.noreply.github.com>
2023-04-22 17:13:09 +0100

    add puck deselection (when hit by other pucks)

--- pucks/test/puck_auto.gd
+++ pucks/test/puck_auto.gd
@@ -4,7 +4,6 @@ extends RigidBody
 signal puck_selected
 
 export var camera_node_path:NodePath
-export var isInteractable:bool
 export var max_push_force:float = 24.0
 export var push_force_multiplier:float = 2.0
 export var push_force:float
@@ -20,7 +19,6 @@ var isSelected:bool = false
 #var isReset:bool = false
 var last_hit:Node = null
 
-onready var camera = get_node(camera_node_path)
 onready var pointer = $Pointer
 
 
@@ -98,11 +96,10 @@ func _integrate_forces(state: PhysicsDirectBodyState) -> void:
 
 func _process(_delta: float) -> void:
 
-	if isDebug:
-		DebugDraw.set_text("puck pos", translation)
-		DebugDraw.set_text("target pos", targetDest)
-		DebugDraw.set_text("Puck last collided with", last_hit)
-		
+#	if isDebug:
+#		DebugDraw.set_text("puck pos", translation)
+#		DebugDraw.set_text("target pos", targetDest)
+#		DebugDraw.set_text("Puck last collided with", last_hit)
 	
 	if isSelected:
 		find_target_pos()
@@ -113,41 +110,11 @@ func find_target_pos() -> void:
 	targetDest = Vector3.ZERO
 	rayOrigin = Vector3.ZERO
 	rayEnd = Vector3.ZERO
-	# get current physics state
-	var space_state = get_world().direct_space_state
-	# get current mouse position in the viewport
-	var mouse_position = get_viewport().get_mouse_position()
-	# set the ray origin
-	rayOrigin = camera.project_ray_origin(mouse_position) 
-	# set the ray end point
-	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000 
-	# get the ray hit
-	var intersection = space_state.intersect_ray(rayOrigin, rayEnd, [], 1)
-#	var intersection = space_state.intersect_ray(rayOrigin, rayEnd)
-	# we want to exclude anything marked on Collision layer 3 "wall" because it messes with the mouse raycast
-	# if there is a proper ray hit get its position and rotate towards it
+
+	var look_here = Vector3(0, translation.y, -1)
 	
-	if isDebug:
-		DebugDraw.set_text("mouse screen pos", mouse_position)
-#		DebugDraw.set_text("intersection", intersection)
-		
-	if not intersection.empty():
-		var pos = Vector3(intersection.position.x, 0.0, intersection.position.z)
-		var look_here = Vector3(pos.x, translation.y, pos.z)
-		
-		if isDebug:
-			DebugDraw.set_text("mouse raycast pos", look_here)
-			if has_node("../MouseMarker"):
-				$"../MouseMarker".set_global_translation(look_here)
-		
-		# need an "offset" for when the puck rotates whilst moving
-		# lock the Angular Y axis for now for prototype
-#			targetDest = translation - look_here
-#		targetDest = Vector3(-look_here.x, look_here.y, -look_here.z) #unary minus operator
-		targetDest = look_here
-#		DebugDraw.draw_line_3d(pos, rayEnd, Color(0, 1, 0))
-		
-#			pointer.look_at(Vector3(targetDest.x, translation.y, targetDest.z), Vector3(0, 1, 0))
+	targetDest = look_here
+	
 	pass
 
 
@@ -156,31 +123,14 @@ func puck_push(direction:Vector3) -> void:
 	apply_impulse(position, direction * push_force)
 
 
-func reset_inertia() -> void:
-	# this is ok if fired only once?
-	set_linear_velocity(Vector3.ZERO)
-#	isReset = true
-
-
 # warning-ignore:unused_argument
 func _unhandled_input(event: InputEvent) -> void:
 	
-	if Input.is_action_just_released("select") && isSelected:
-#		print("deselect")
-		pointer.visible = false
-		isSelected = false
-		puck_push(cur_dir)
-
-
-# warning-ignore:unused_argument
-func _on_Puck_input_event(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	
-#	if self == get_parent().current_selected_puck:
-#		print("currently selected puck")
-	
-	if Input.is_action_just_pressed("select"):
-#		print("select")
-		pointer.visible = true
+	if Input.is_action_just_pressed("puck"):
 		isSelected = true
-#		emit_signal("puck_selected", self)
+	
+	if Input.is_action_just_released("puck"):
+		puck_push(cur_dir)
+		isSelected = false
 
