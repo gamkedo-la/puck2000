f190a7230adbd76cf8fe1d17dd40b26c0af731bc
liyiz <5160472+liyiz@users.noreply.github.com>
2023-05-06 14:10:16 +0100

    scene management setup
    - Main.tscn renamed to SceneManager.tscn
    - scene_manager.gd removed as autoload
    - add game_scene.gd as GameScene class
    - built-in scene script for TitleScreen as test
    - SceneManager.tscn is default run scene

--- managers/scene_manager.gd
+++ managers/scene_manager.gd
@@ -2,9 +2,22 @@ extends Node
 # Manages the scenes and scene transitions, 
 # so this will be working at the Main.tscn level
 
-#onready var scene_anim = $"../Main/AnimationPlayer"
+onready var scene_anim = $TransitionPlayer
+
+var current_scene = null
+
 
 func _ready() -> void:
-#	scene_anim.play("fade_in")
-	pass # Replace with function body.
+	current_scene = $TitleScreen
+	current_scene.connect("scene_changed", self, "handle_scene_change")
+	scene_anim.play("fade_in")
+	print("current scene: ", current_scene.name)
+
+
+func handle_scene_change(current_scene_name:String, requested_scene_name:String) -> void:
+	pass
+
+
+func cleanup() -> void:
+	pass
 
