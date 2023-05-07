23708e24a345a9d7206dcb6be34ca61b4c0c28df
liyiz <5160472+liyiz@users.noreply.github.com>
2023-05-07 16:36:15 +0100

    fix select screen pos, puck scene node renames, clamp puck selection height

---
+++ scenes/MenuGameSetup/camera_axis.gd
@@ -0,0 +1,46 @@
+extends Spatial
+
+export var interval_value:float = 2.0
+
+var isInputEnabled:bool = true
+var start_height:float
+var max_depth:float
+var current_height:float
+
+
+func _ready() -> void:
+	var factor = $"../Pucks".get_children().size() - 1
+	var height = interval_value * factor * -1.0
+	current_height = get_global_transform().origin.y
+	start_height = current_height
+	max_depth = current_height + height
+#	prints(factor, max_depth)
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	
+	if Input.is_action_pressed("ui_down") && isInputEnabled && current_height > max_depth:
+#		print(transform.origin.y)
+		isInputEnabled = false
+		var tween = get_tree().create_tween()
+		var value = get_global_transform().origin - Vector3(0, interval_value, 0)
+		tween.tween_property(self, "translation", value, 0.25).set_trans(Tween.TRANS_SINE)
+		tween.connect("finished", self, "_enable_input")
+
+	if Input.is_action_pressed("ui_up") && isInputEnabled && current_height < start_height:
+#		print(transform.origin.y)
+		isInputEnabled = false
+		var tween = get_tree().create_tween()
+		var value = get_global_transform().origin + Vector3(0, interval_value, 0)
+		tween.tween_property(self, "translation", value, 0.25).set_trans(Tween.TRANS_SINE)
+		tween.connect("finished", self, "_enable_input")
+
+
+func _enable_input() -> void:
+	isInputEnabled = true
+	current_height = get_global_transform().origin.y
+
+
+
+
+
