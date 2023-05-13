368c55fa8782679d758aec4df9c12031ca6eeab1
liyiz <5160472+liyiz@users.noreply.github.com>
2023-05-13 13:37:34 +0100

    add sakura standard skin
    some minor edits included

--- scenes/MenuGameSetup/menu_game_setup.gd
+++ scenes/MenuGameSetup/menu_game_setup.gd
@@ -2,9 +2,21 @@ extends Spatial
 
 onready var btn_startgame = $Interface/CanvasLayer/Button
 
+var selections = {}
+
 
 func _ready() -> void:
 	btn_startgame.connect("pressed", self, "goto_gameplay")
+
+
+func select_table() -> void:
+	pass
+
+
+func select_puck(selected_puck:String) -> void:
+	selections["puck"] = selected_puck
+	print(selections)
+	pass
 
 
 func goto_gameplay() -> void:
