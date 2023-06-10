38c3c4124ef0475516e5b2dcb2606524cdd1be44
liyiz <5160472+liyiz@users.noreply.github.com>
2023-06-10 16:03:01 +0100

    puck select btn  is now text based

---
+++ scenes/MenuGameSetup/SelectPucks/puckselect_btn.gd
@@ -0,0 +1,16 @@
+extends TextureButton
+
+export(String) var text = "Button text"
+
+func _ready() -> void:
+	setup_text()
+	show_arrows()
+	set_focus_mode(true)
+
+
+func setup_text() -> void:
+	$Label.text = text
+
+
+func show_arrows() -> void:
+	pass
