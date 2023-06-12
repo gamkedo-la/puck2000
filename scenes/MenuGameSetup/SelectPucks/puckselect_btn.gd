71d51a72c36cc6bbc15f491bc33ec01ff6f9a70b
liyiz <5160472+liyiz@users.noreply.github.com>
2023-06-12 13:26:10 +0100

    add keyboard/gamepad focus puck select buttons

--- scenes/MenuGameSetup/SelectPucks/puckselect_btn.gd
+++ scenes/MenuGameSetup/SelectPucks/puckselect_btn.gd
@@ -6,6 +6,24 @@ func _ready() -> void:
 	setup_text()
 	show_arrows()
 	set_focus_mode(true)
+	
+	# check if button is first in layout, and grab focus
+	var parent = get_parent()
+	if parent:
+		var isFirst = get_index() == 0
+		if isFirst:
+			grab_focus()
+	
+	# set focus neighbours relative to button
+	var self_index = get_index()
+	
+	# get node path of the prev and next buttons
+	if parent.get_child(self_index - 1):
+		var prev_btn = parent.get_child(self_index - 1).get_path()
+		set_focus_neighbour(MARGIN_TOP, prev_btn)
+	if parent.get_child(self_index + 1):
+		var next_btn = parent.get_child(self_index + 1).get_path()
+		set_focus_neighbour(MARGIN_BOTTOM, next_btn)
 
 
 func setup_text() -> void:
