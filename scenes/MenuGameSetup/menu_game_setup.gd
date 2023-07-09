2693c412a5d8bba754b4fbbf60db4de6d65b3a9a
liyiz <5160472+liyiz@users.noreply.github.com>
2023-07-09 11:22:13 +0100

    add button checks for game setup
    need to add checks or highlight for selected table and puck

--- scenes/MenuGameSetup/menu_game_setup.gd
+++ scenes/MenuGameSetup/menu_game_setup.gd
@@ -20,6 +20,7 @@ export(Array, PackedScene) var puck_cosmetics = [
 ]
 onready var btn_next = $Interface/CanvasLayer/ButtonNext
 onready var btn_prev = $Interface/CanvasLayer/ButtonPrev
+onready var btn_play = $Interface/CanvasLayer/ButtonPlay
 onready var interface_screen = $Interface
 
 var current_select_screen = 0
@@ -35,6 +36,8 @@ func _ready() -> void:
 #	btn_startgame.connect("button_up", self, "goto_gameplay")
 	btn_next.connect("button_up", self, "goto_animation", [true])
 	btn_prev.connect("button_up", self, "goto_animation", [false])
+	btn_play.connect("button_up", self, "goto_gameplay")
+	
 	connect("send_selections", $"../SceneTransition/DataBus", "test_func")
 	
 	for child in $Interface/Slides/SelectTables.get_children():
@@ -46,6 +49,9 @@ func _ready() -> void:
 	
 	# connect signals from "Select" nodes
 	# create function to add Node to array to send to Gameplay scene
+	
+	# reset buttons
+	sort_buttons()
 
 
 func _on_viewport_container_clicked(container:ViewportContainer, index:int, type:String) -> void:
@@ -88,13 +94,42 @@ func goto_animation(isNext:bool) -> void:
 	
 	if isNext == true:
 		value = slides.get_position() - Vector2(interface_screen.get_viewport_rect().size.x, 0)
+		# track which screen we're on:
+		current_select_screen += 1
 	else:
 		value = slides.get_position() + Vector2(interface_screen.get_viewport_rect().size.x, 0)
+		# track which screen we're on:
+		current_select_screen -= 1
+	
+	sort_buttons()
 	
 	isInputEnabled = false
 	tween.tween_property(slides, "rect_position", value, 0.5).set_trans(Tween.TRANS_SINE)
 	tween.connect("finished", self, "_enable_input")
 	pass
+
+
+func sort_buttons() -> void:
+	# show/hide buttons based on current screen
+	# use script global, current_select_screen
+	match current_select_screen:
+		0:
+			btn_next.show() 
+			btn_prev.hide()
+			btn_play.hide()
+			pass
+		1:
+			btn_next.show() 
+			btn_prev.show()
+			btn_play.hide()
+			pass
+		2:
+			btn_next.hide() 
+			btn_prev.show()
+			btn_play.show()
+			pass
+		_:
+			print_debug("Did something go wrong in sort_buttons()?")
 
 
 func goto_gameplay() -> void:
